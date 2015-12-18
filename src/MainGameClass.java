78bf69e5d12978d65c42e20b2a3d4e6f02442d38
teswolf96 <teswolf96@gmail.com>
2015-12-18 15:32:28 -0600

    Moved to github

--- src/MainGameClass.java
+++ src/MainGameClass.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 /**
  * Name: Thomas Shaw
  */
-//Now with git!
+//Moved to github
 public class MainGameClass {
     public static void main(String[] args) {
         System.out.println("Welcome to A New Adventure!");
