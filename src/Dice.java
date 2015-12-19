93375f74c59f3f859bcaec125887f8c89c50b482
teswolf96 <teswolf96@gmail.com>
2015-12-18 23:02:36 -0600

    Improved randomness

--- src/Dice.java
+++ src/Dice.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Dice {
 
 	public static int roll(int sides){
-		Random diceRoller = new Random(System.nanoTime() * System.nanoTime());
+		Random diceRoller = new Random(System.nanoTime() * System.currentTimeMillis());
 		return diceRoller.nextInt(sides)+1;
 	}
 }
